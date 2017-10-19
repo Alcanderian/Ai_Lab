73b7615052e9bf3d7bdf34e299a82b81e17141d7
Alcanderian <alcanderian@gmail.com>
2017-10-19 21:53:36 +0800

    init

---
+++ Pla/build_mat.py
@@ -0,0 +1,8 @@
+import os
+import glob
+
+
+for csv in glob.glob(r'*.csv'):
+    with open(csv, 'r') as src:
+        with open(csv[:-4] + '.mat', 'w') as dst:
+            dst.write(src.read().replace(',', ' ').replace('?', '0'))
